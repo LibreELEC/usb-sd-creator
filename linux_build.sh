931ac5d5838a75a338b910c60b17c668fcbb27a6
Lukas Rusak <lorusak@gmail.com>
2023-07-01 22:04:53 -0700

    linux: update linux_build.sh for cmake build
    Signed-off-by: Lukas Rusak <lorusak@gmail.com>

--- linux_build.sh
+++ linux_build.sh
@@ -21,25 +21,18 @@ set -e
 
 echo ""
 echo "Building 64 bit version..."
-QT_DIR=~/Qt6.2.2/6.2-static-64
-
-export PATH=$QT_DIR/bin:$QT_DIR/../6.2.2/gcc_64/bin:$PATH
 
 echo ""
-echo "Creating .qm files"
-lrelease creator.pro
-
-echo ""
-echo "Running qmake..."
-qmake
+echo "Running cmake..."
+cmake -B build
 
 echo ""
 echo "Building..."
-make -j4
+cmake --build build --parallel
 
 echo ""
-echo "  To run application directly type"
-echo "    sudo ./LibreELEC.USB-SD.Creator.Linux.bin"
+echo "  Run application"
+echo "    ./build/LibreELEC.USB-SD.Creator.Linux.bin"
 echo ""
 echo "Finished..."
 echo ""
