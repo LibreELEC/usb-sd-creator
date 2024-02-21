fdd6f8b5edac293c55f071cea9229420b306349e
Christian Hewitt <christianshewitt@gmail.com>
2024-02-21 05:31:07 +0000

    lang: align master template with transifex created .ts files
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- lang/lang-en_GB.ts
+++ lang/lang-en_GB.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="en_GB">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="en_GB">
 <context>
     <name>Creator</name>
     <message>
