e248a3976729846c1513a9a7dc7a262918b47d4d
Andrey Filipenkov <decapitator@ukr.net>
2024-02-23 15:45:09 +0300

    [CI] macOS: prepare secret files

--- dmg_osx/notarize.sh
+++ dmg_osx/notarize.sh
@@ -9,14 +9,14 @@ fi
 echo 'sign dmg'
 codesign --verbose=4 --force --sign "$CODE_SIGN_IDENTITY" "$dmgPath"
 
-if [[ -z "$ASC_API_KEY" || -z "$ASC_API_KEY_ID" || -z "$ASC_API_KEY_ISSUER" || -z "$ASC_TEAM_ID" ]] ; then
-  echo 'skip dmg notarization - ASC_API_KEY / ASC_API_KEY_ID / ASC_API_KEY_ISSUER / ASC_TEAM_ID not provided'
+if [[ -z "$ASC_API_KEY_PATH" || -z "$ASC_API_KEY_ID" || -z "$ASC_API_KEY_ISSUER" || -z "$ASC_TEAM_ID" ]] ; then
+  echo 'skip dmg notarization - ASC_API_KEY_PATH / ASC_API_KEY_ID / ASC_API_KEY_ISSUER / ASC_TEAM_ID not provided'
   exit 0
 fi
 echo 'notarize dmg'
 xcrun notarytool submit \
   --verbose \
-  --key "$ASC_API_KEY" \
+  --key "$ASC_API_KEY_PATH" \
   --key-id "$ASC_API_KEY_ID" \
   --issuer "$ASC_API_KEY_ISSUER" \
   --team-id "$ASC_TEAM_ID" \
